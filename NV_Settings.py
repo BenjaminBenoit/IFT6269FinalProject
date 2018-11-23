9f5fb6623b471b83895e14977782d393ba96dc03
Vachonni <nicholasvachon@hotmail.com>
2018-11-22 21:45:07 -0500

    Nicholas versions. Add: valid set, convergence criteria, save.

---
+++ NV_Settings.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+
+This file contains all the Settings (constants) used accross the application
+
+"""
+
+
+class Settings:
+    
+    DATASET_PATH='./data'
+    
+    TRAINING_BATCH_SIZE = 128
+    VALID_BATCH_SIZE = 128
+    TESTING_BATCH_SIZE = 128
+    
+    LOG_INFORMATION_INTERVAL = 1000
+    
+    NUMBER_OF_EPOCH = 3
+    LOSSVARIATION = 0.0001         # Stop training if variation is smaller than it
+    
+    LEARNING_RATE = 0.001
+    
+    # Whether or not the model should run on the CPU or GPU
+    # If DEVICE="cpu" then the model run on CPU
+    # If DEVICE="cuda" then the model run on GPU
+    DEVICE = "cpu"
+    
+    # Note : this parameter is only used if DEVICE="cuda"
+    # Beware of the memory usage when setting this parameter
+    NUMBER_OF_WORKERS = 1
+    
+    # Note : this parameter is only used if DEVICE="cuda"
+    # See documentation : https://pytorch.org/docs/master/notes/cuda.html
+    # ('Use pinned memory buffers' part)
+    # If execution of the program freeze, put this parameter to false
+    PIN_MEMORY = True
--- NV_VAE_Training.py
+++ NV_VAE_Training.py
@@ -14,20 +14,21 @@ PROJECT
 
 ######## IMPORT
 import torch
+import numpy as np
 from IWAE import VAE
-from Util import Util
+from NV_Util import Util
 from torch import optim
-from Settings import Settings
+from NV_Settings import Settings
 from torchvision import datasets, transforms
 
-
-
-######## MAIN
 
 
 Util.checkDeviceAndCudaAvailability()
 
 currentTransform = transforms.ToTensor()
+
+
+######## LOAD DATA
 
 # First time running this code, it is needed to put the download value to True instead of False
 # Ensure that an empy folder named 'data' is created in the same folder as this file source code
@@ -38,16 +39,53 @@ kwargs = {}
 if(Settings.DEVICE == "cuda"):
     kwargs = {'num_workers': Settings.NUMBER_OF_WORKERS, 'pin_memory': Settings.PIN_MEMORY}
 
-trainLoader = torch.utils.data.DataLoader(currentTrainingDataset, batch_size=Settings.TRAINING_BATCH_SIZE, shuffle=True, **kwargs)
+# Split the train set in 90% train set and 10% valid set)
+trainLoader, validLoader = Util.splitTrainSet(currentTrainingDataset, ratio=0.1)
 testLoader = torch.utils.data.DataLoader(currentTestingDataset, batch_size=Settings.TESTING_BATCH_SIZE, shuffle=True, **kwargs)
+
+
+######## CREATE MODEL
 
 modelVAE = VAE().to(Settings.DEVICE)
 optimizer = optim.Adam(modelVAE.parameters(), lr=Settings.LEARNING_RATE)
 
+
+
+######## TRAIN
+
+# Keep loosses at each Epoch [[train], [valid], [test]]
+losses = [[],[],[]]
+qt_epoch = 0
+
 for indexEpoch in range(1, Settings.NUMBER_OF_EPOCH+1):
-    Util.train(modelVAE, trainLoader, optimizer, indexEpoch)
-    Util.test(modelVAE, testLoader, indexEpoch)
+    
+    losses[0].append(Util.train(modelVAE, trainLoader, optimizer, indexEpoch))
+    losses[1].append(Util.eval(modelVAE, validLoader, indexEpoch, "Valid"))
+    losses[2].append(Util.eval(modelVAE, testLoader, indexEpoch, "Test"))
+    print(losses)
+    qt_epoch = indexEpoch                   #To be able to save 
+    
+    # If at least 2 losses 
+    if indexEpoch >= 2:
+        train_loss_variation = abs(losses[0][-1] - losses[0][-2])
+        # Stop if train loss variation between 2 epoch smaller Settings.LOSSVARIATION
+        if train_loss_variation < Settings.LOSSVARIATION: 
+            break
 
+    
+######## SAVE 
+ 
+info = {'qt_epoch': qt_epoch, 'lr': Settings.LEARNING_RATE, \
+        'batch_size': Settings.TRAINING_BATCH_SIZE,\
+        'losses': losses}       
+np.save('model_info.npy', losses)
 
-
+# Save trained model
+torch.save(modelVAE.state_dict(), "model_state_dict.pth")
+    
+    
+    
+    
+    
+    
     
