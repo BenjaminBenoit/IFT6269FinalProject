e22242ac5c271107d251fd920f6a6773adf23818
Vachonni <nicholasvachon@hotmail.com>
2018-11-23 18:24:21 -0500

    Back to one version

--- Settings.py
+++ Settings.py
@@ -10,19 +10,21 @@ class Settings:
     
     DATASET_PATH='./data'
     
-    TRAINING_BATCH_SIZE = 1
-    TESTING_BATCH_SIZE = 1
+    TRAINING_BATCH_SIZE = 128
+    VALID_BATCH_SIZE = 128
+    TESTING_BATCH_SIZE = 128
     
-    LOG_INFORMATION_INTERVAL = 10
+    LOG_INFORMATION_INTERVAL = 1000
     
-    NUMBER_OF_EPOCH = 1
+    NUMBER_OF_EPOCH = 1000
+    LOSSVARIATION = 0.0001         # Stop training if variation is smaller than it
     
     LEARNING_RATE = 0.001
     
     # Whether or not the model should run on the CPU or GPU
     # If DEVICE="cpu" then the model run on CPU
     # If DEVICE="cuda" then the model run on GPU
-    DEVICE = "cpu"
+    DEVICE = "cuda"
     
     # Note : this parameter is only used if DEVICE="cuda"
     # Beware of the memory usage when setting this parameter
--- Util.py
+++ Util.py
@@ -13,8 +13,11 @@ UTILIY CLASS
 
 ######## IMPORT
 import torch
-from Settings import Settings
+from NV_Settings import Settings
 import torch.nn.functional as Functional
+import numpy as np
+from torch.utils.data.sampler import SubsetRandomSampler
+
 
 ######## UTIL
 
@@ -25,11 +28,17 @@ class Util:
         if Settings.DEVICE == "cuda" and not torch.cuda.is_available():
             print("Settings.DEVICE specify a GPU computation but CUDA is not available")
             raise
+  
+    
+    
     
     def train(model, trainLoader, optimizer, epoch):
         model.train()
         trainLoss = 0
+        qt_data = 0
+        
         for batchIndex, (data, target) in enumerate(trainLoader):
+            qt_data += len(data)
             data = data.to(Settings.DEVICE)
             # re-initialize the gradient computation
             optimizer.zero_grad()
@@ -38,30 +47,36 @@ class Util:
             loss.backward()
             trainLoss += loss.item()
             optimizer.step()
-            if batchIndex % Settings.LOG_INFORMATION_INTERVAL == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batchIndex * len(data), len(trainLoader.dataset),
-                    100. * batchIndex / len(trainLoader), loss.item()))
-                
-        print('====> Epoch: {} Average loss: {:.4f}'.format(
-              epoch, trainLoss / len(trainLoader.dataset)))
+        
+        trainLoss /= qt_data     
+        print('\n==> Epoch: {} \n======> Train loss: {:.4f}'.format(
+              epoch, trainLoss))
+        
+        return trainLoss
         
         
-    def test(model, testLoader, epoch):
+        
+    def eval(model, Loader, epoch, setType):         # SetType is 'Valid' or 'Test'
         model.eval()
-        testLoss = 0
+        lossCumul = 0
+        qt_data = 0
         
         # No gradient descent is being made during testing
         with torch.no_grad():
-            for index, (data, _) in enumerate(testLoader):
+            for index, (data, _) in enumerate(Loader):
+                qt_data += len(data)
                 data = data.to(Settings.DEVICE)
                 posteriorResults, mu, logvar = model(data)
-                loss = Util.calculateLoss(posteriorResults, mu, logvar, data)             
-                testLoss += loss.item()
-
-        testLoss /= len(testLoader.dataset)
-        print('====> Test set loss: {:.4f}'.format(testLoss))
+                loss = Util.calculateLoss(posteriorResults, mu, logvar, data) 
+                lossCumul += loss.item()
+                
+        lossCumul /= qt_data
+        print('======> {} loss: {:.4f}'.format(setType, lossCumul))
         
+        return lossCumul
+        
+    
+    
         
     # The objective function is made of two terms
     # First term is the KL divergence between two distributions : Q(z|X) and P(z|X)
@@ -80,4 +95,65 @@ class Util:
     def createGraphic(titleFigure, fileName, data):
         print("Todo")
         
+        
+        
+    
+    def splitTrainSet(train_dataset, ratio=0.1):
+        """
+        Further splitting our training dataset into training and validation sets.
+        In: A Pytorch Dataset object and a ratio for valid size 
+        Out: Dataloader for train and Dataloader for valid
+        
+        """
+        # Define the indices
+        indices = list(range(len(train_dataset)))      # start with all the indices in training set
+        split = int(len(train_dataset) * ratio)        # define the split size
+                
+        # Random, non-contiguous split
+        validation_idx = np.random.choice(indices, size=split, replace=False)
+        train_idx = list(set(indices) - set(validation_idx))
+        
+        # Contiguous split
+        # train_idx, validation_idx = indices[split:], indices[:split]
+        
+        # define our samplers -- we use a SubsetRandomSampler because it will return
+        # a random subset of the split defined by the given indices without replacement
+        train_sampler = SubsetRandomSampler(train_idx)
+        validation_sampler = SubsetRandomSampler(validation_idx)
+        
+        # Create the train_loader -- use your real batch_size which you
+        # I hope have defined somewhere above
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                                   batch_size=Settings.TRAINING_BATCH_SIZE, 
+                                                   sampler=train_sampler)
+        
+        # You can use your above batch_size or just set it to 1 here.  Your validation
+        # operations shouldn't be computationally intensive or require batching.
+        validation_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                                        batch_size=Settings.VALID_BATCH_SIZE, 
+                                                        sampler=validation_sampler)
+        
+        return train_loader, validation_loader
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
